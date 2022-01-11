3c9d0611f2bffb23cd9a9a0555f8826bbf0e6e98
UlianaOr <Ouliana9@gmail.com>
2022-01-11 12:12:10 +0100

    qwe2

---
+++ script.js
@@ -0,0 +1,15 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+bird.src = "img\flappy_bird_bird.png";
+bg.src = "img\flappy_bird_bg.png";
+fg.src = "img\flappy_bird_fg.png";
+pipeUp.src = "img\flappy_bird_pipeUp.png";
+pipeBottom.src = "img\flappy_bird_pipeBottom.png";
+
